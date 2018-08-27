2292a35eb3a106de2dff7774728a9922d5d19273
Jake Musich <makejusich@gmail.com>
2018-08-27 13:15:17 -0400

    readme, file structure cleaned, devdepends installed, assets added

---
+++ src/utils/copy.js
@@ -0,0 +1,12 @@
+export const copy = {
+  donationSection: {
+    header: 'Donate Today',
+    subheader: 'Help protect the planet today, become a Little Zebra ambassador',
+    content: null
+  },
+  infoSection: {
+    header: 'What we do',
+    subheader: 'Discover more about the Little Zebra foundation and how you can help us',
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet, magna sit amet suscipit laoreet, ante velit scelerisque massa, ac mattis ex magna et turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam efficitur tellus vitae erat laoreet sodales. Donec mollis, augue nec feugiat accumsan, ante dui aliquet ante, sit amet vestibulum ligula ex non mi. Nunc quis imperdiet leo, blandit maximus orci. Phasellus porttitor test eros magna, et viverra massa cursus ut. Cras nec est et urna ullamcorper volutpat et tristique augue. Nullam sit amet odio eget tortor rhoncus volutpat nec sed ipsum. Vivamus semper a ligula a sodales. Morbi mi dui, luctus ac vestibulum in, dapibus id ante. Vivamus eu odio non mi finibus dapibus a a erat. Cras commodo pharetra sapien, non ultrices nisl egestas eu."
+  }
+}
